733a6bd13742d5ceec1b09ee32a71e3494c763e9
Roberto <roberto30nembhwani@gmail.com>
2022-11-07 14:00:02 +0700

    Added modal-information

---
+++ Components/CompleteDeleteCard.js
@@ -0,0 +1,10 @@
+const CompleteDeleteCard = () => {
+  return (
+    <div className="completeDeleteCard" data-cy="modal-information">
+      <img src="/modal-information-icon.png" alt="" />
+      <h2>Activity berhasil dihapus</h2>
+    </div>
+  );
+};
+
+export default CompleteDeleteCard;
--- Components/ConfirmDeleteModal.js
+++ Components/ConfirmDeleteModal.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import { currentDelete } from '../store';
 import { activityStore } from '../store';
+import CompleteDeleteCard from './CompleteDeleteCard';
+
 const ConfirmDeleteModal = () => {
   const currentDeleteTarget = currentDelete(
     (state) => state.currentDeleteTarget
   );
   const deleteActivity = activityStore((state) => state.deleteActivity);
   const disableModal = currentDelete((state) => state.disableModal);
+
+  const [completeDelete, setCompleteDelete] = useState(false);
 
   const removeElement = (e) => {
     fetch(
@@ -15,38 +20,42 @@ const ConfirmDeleteModal = () => {
       }
     )
       .then(deleteActivity(currentDeleteTarget.id))
-      .then(disableModal());
+      .then(setCompleteDelete(true));
   };
   return (
     <div className="deleteModalContainer" onClick={disableModal}>
-      <div
-        className="deleteModalCard"
-        onClick={(e) => e.stopPropagation()}
-        data-cy="todo-modal-delete"
-      >
-        <img src="delete.png" alt="" data-cy="modal-delete-icon" />
+      {completeDelete ? (
+        <CompleteDeleteCard />
+      ) : (
+        <div
+          className="deleteModalCard"
+          onClick={(e) => e.stopPropagation()}
+          data-cy="modal-delete"
+        >
+          <img src="delete.png" alt="" data-cy="modal-delete-icon" />
 
-        <h2 data-cy="modal-delete-title">
-          Apakah anda yakin menghapus activity
-          <span>{`"${currentDeleteTarget.title}?"`}</span>
-        </h2>
-        <div className="buttonContainer">
-          <button
-            className="batal"
-            onClick={disableModal}
-            data-cy="modal-delete-cancel-button"
-          >
-            Batal
-          </button>
-          <button
-            className="hapus"
-            onClick={removeElement}
-            data-cy="modal-delete-confirm-button"
-          >
-            Hapus
-          </button>
+          <h2 data-cy="modal-delete-title">
+            Apakah anda yakin menghapus activity
+            <span>{`"${currentDeleteTarget.title}?"`}</span>
+          </h2>
+          <div className="buttonContainer">
+            <button
+              className="batal"
+              onClick={disableModal}
+              data-cy="modal-delete"
+            >
+              Batal
+            </button>
+            <button
+              className="hapus"
+              onClick={removeElement}
+              data-cy="modal-delete-confirm-button"
+            >
+              Hapus
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
